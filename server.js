376fbb9551b57f6cc76eb5295a9dd1240a37c123
Harsha Narayana <harsha.narayana@ge.com>
2018-05-30 10:27:23 +0530

    fix server starter

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+
 const app = express();
 
 const forceSSL = function() {
@@ -12,10 +14,15 @@ const forceSSL = function() {
 };
 
 app.use(forceSSL());
-app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res) {
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
