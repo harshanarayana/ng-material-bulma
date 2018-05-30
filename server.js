ba8c4f3a0512efe2340ebcc3c63b8caf1073761e
Harsha Narayana <harsha.narayana@ge.com>
2018-05-30 10:22:56 +0530

    fix server mapping order

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ const forceSSL = function() {
 
 app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+app.listen(process.env.PORT || 8080);
